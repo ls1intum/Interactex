32900c0d50e0a37437ccaa0595d0109b5a3bee79
Juan Haladjian <avenix@gmail.com>
2014-01-16 14:45:53 +0100

    FIxed Headers iFirmata and eGMP modified Project name in eGMP fixed some compiler warnings for x64 on eGMP

--- iFirmata/iFirmata/code/IFPinsController.m
+++ iFirmata/iFirmata/code/IFPinsController.m
@@ -1,10 +1,30 @@
-//
-//  IFFirmata.m
-//  iFirmata
-//
-//  Created by Juan Haladjian on 6/28/13.
-//  Copyright (c) 2013 TUM. All rights reserved.
-//
+/*
+IFPinsController.m
+iFirmata
+
+Created by Juan Haladjian on 28/06/2013.
+
+iFirmata is an App to control an Arduino board over Bluetooth 4.0. iFirmata uses the Firmata protocol: www.firmata.org
+
+www.interactex.org
+
+Copyright (C) 2013 TU Munich, Munich, Germany; DRLab, University of the Arts Berlin, Berlin, Germany; Telekom Innovation Laboratories, Berlin, Germany
+	
+Contacts:
+juan.haladjian@cs.tum.edu
+katharina.bredies@udk-berlin.de
+opensource@telekom.de
+
+    
+It has been created with funding from EIT ICT, as part of the activity "Connected Textiles".
+
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #import "IFPinsController.h"
 #import "IFPin.h"
