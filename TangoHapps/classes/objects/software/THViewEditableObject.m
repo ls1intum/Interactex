a288090fd7a057b2598e1d43ca6a8ad05515d723
Juan Haladjian <avenix@gmail.com>
2014-02-21 19:17:05 +0100

    fixed bug iphone elements being rescaled with layer fixed bug iphone elements dropped at wrong positions when layer zoomed

--- TangoHapps/classes/objects/software/THViewEditableObject.m
+++ TangoHapps/classes/objects/software/THViewEditableObject.m
@@ -56,7 +56,7 @@ You should have received a copy of the GNU General Public License along with thi
 -(void) loadView {
 
     self.canChangeBackgroundColor = YES;
-    
+    self.canBeScaled = NO;
     self.canBeRootView = YES;
     _subviews = [NSMutableArray array];
     self.minSize = kDefaultViewMinSize;
