ca950dfa6e0c3739ed34d09c99676c966432ee68
Timm Beckmann <beckmann@in.tum.de>
2014-11-20 11:37:35 +0100

    invisible right

--- TangoHapps/classes/objects/gestures/THGestureEditableObject.m
+++ TangoHapps/classes/objects/gestures/THGestureEditableObject.m
@@ -347,7 +347,7 @@
     [self addChild:self.sprite z:-10];
     self.z = kGestureObjectZ;
     for (TFEditableObject * obj in _attachments) {
-        obj.visible = true;
+        obj.visible = false;
     }
 }
 
